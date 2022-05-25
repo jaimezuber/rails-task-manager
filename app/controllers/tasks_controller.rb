aa66741cab3e5b95ba9a1d0cba8dca288e74c1e6
jaime zuber <jaimezuber99@gmail.com>
2022-05-25 19:48:36 -0300

    msj

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
 
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -17,10 +16,25 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit; end
+
+  def update
+    @task.update(restaurant_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def restaurant_params
     params.require(:task).permit(:title, :details)
   end
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
