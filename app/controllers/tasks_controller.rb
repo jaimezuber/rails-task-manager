d18ba9318808e93283bd2fbc2a919f0becad77e8
jaime zuber <jaimezuber99@gmail.com>
2022-05-24 23:04:01 -0300

    hasta editar una tarea

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,26 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(restaurant_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
